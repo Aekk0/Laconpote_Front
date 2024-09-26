17fc034ef88bce6b964976bd2acd78ee0c48d332
Nicolas Hallaert <n.hallaert@myunisoft.fr>
2024-09-26 15:52:53 +0200

    feature(./app/services/basket/basket): base for basket service

---
+++ src/app/services/basket/basket.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BasketService {
+  currentBasketSubject = new BehaviorSubject(null);
+
+  constructor() { }
+
+  public async update(products: [Record<string, any> & { quantity: number; }]) {
+    let currents = null;
+
+    this.getCurrentBasket().subscribe((basket) => currents = basket);
+
+    const result = [];
+
+    for (const product of products) {
+      const sameProduct = currents!.find((current: any) => current.id === product["id"]);
+
+      if (!sameProduct) {
+        result.push(product);
+
+        continue;
+      }
+
+      result.push({
+        ...product,
+        quantity: product.quantity + sameProduct.quantity
+      });
+    }
+
+    this.setBasket(result);
+  }
+
+  getCurrentBasket(): Observable<any> {
+    return this.currentBasketSubject.asObservable();
+  }
+
+  setBasket(basket: any): void {
+    this.currentBasketSubject.next(basket);
+  }
+}
