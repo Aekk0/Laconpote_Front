e723fddd8fc69882dcda1035865a3c68606f02b0
Aekk0 <Franck.Hallaert@gmail.com>
2024-09-27 05:42:43 +0200

    feature(): add module to connect with css

--- src/app/features/login/login.component.ts
+++ src/app/features/login/login.component.ts
@@ -1,27 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { NgModule } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
 
+import {MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { CommonModule } from '@angular/common';
+
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+  login: boolean = true;
   password: string = "";
+  confirmPassword: string = "";
   email: string = "";
 
-  constructor(private authService: AuthService) {
+  constructor(
+    private authService: AuthService,
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    @Inject(MatDialogRef) public dialogRef: MatDialogRef<LoginComponent>
+  ) {
     
   }
 
+  changeState() {
+    if (this.login === true) {
+      this.login = false;
+      
+      return;
+    }
+
+    this.login = true;
+  }
+
   async onSubmitForm(form: NgForm): Promise<void> {
-
     await this.authService.authenticate(form.value);
+    if (form.valid && this.password === this.confirmPassword) {
+      console.log('Form submitted successfully!');
+    } else {
+      console.error('Form invalid or passwords do not match');
+    }
+  }
 
+  onNoClick(): void {
+    this.dialogRef.close();
   }
 }
--- src/app/navigation/cookie/header/header.component.ts
+++ src/app/navigation/cookie/header/header.component.ts
@@ -1,6 +1,10 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { AuthService } from '../../../services/auth.service';
+
+import {MatDialog} from '@angular/material/dialog';
+import { LoginComponent } from '../../../features/login/login.component';
+import { BasketService } from '../../../services/basket/basket.service';
 
 @Component({
   selector: 'app-header',
@@ -12,7 +16,23 @@ import { AuthService } from '../../../services/auth.service';
 export class HeaderComponent {
   user: any;
 
-  constructor(private authService: AuthService) {
+  constructor(
+    private authService: AuthService,
+    private basketService: BasketService,
+    @Inject(MatDialog) public dialog: MatDialog
+  ) {
     this.authService.getCurrentUser().subscribe((user) => this.user = user);
+  }
+
+  openLogin(): void {
+    const dialogRef = this.dialog.open(LoginComponent, {
+      width: "500px",
+    });
+  }
+
+  openBasket(): void {
+    const dialogRef = this.dialog.open(LoginComponent, {
+      width: "500px"
+    });
   }
 }
